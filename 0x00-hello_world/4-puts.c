e9af4a7dad24084e4a35fb64a8fa204fca8e9cf3
Soly-M-27 <solymar227@icloud.com>
2021-09-30 19:55:55 +0000

    Write a line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+int main()
+{
+	//string initialization
+	char Line[] = "Programming is like building a multilingual puzzle\n";
+	puts(Line); // writing the string to stdout
+
+	return 0;
+}
