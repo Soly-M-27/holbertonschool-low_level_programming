99004ce41739ae893550f6bb26119d4630af5b54
Soly-M-27 <solymar227@icloud.com>
2021-09-30 20:20:22 +0000

    Write a string using puts in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	puts('"');
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 
 	getchar();
 
