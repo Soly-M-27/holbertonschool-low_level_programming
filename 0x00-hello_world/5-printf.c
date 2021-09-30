a18b84bfd5a4fc446c6d0fcbcb397dc3c51a0295
Soly-M-27 <solymar227@icloud.com>
2021-09-30 20:45:53 +0000

    Write a string using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-int main (void)
+/**
+ * main - Prints a string using printf
+ * Return: Returns 0
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
