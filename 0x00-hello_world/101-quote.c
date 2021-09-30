d02348f76ad8926460ad1eacbb2f4cdc34ecd642
Soly-M-27 <solymar227@icloud.com>
2021-09-30 21:44:31 +0000

    Write string without puts or printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Prints a string without using puts or printf
+ * Return: Returns 1
+ */
+int main(void)
+{
+
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19,");
+
+	return (1);
+}
