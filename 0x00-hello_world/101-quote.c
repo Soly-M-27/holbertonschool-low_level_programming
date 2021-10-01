e5aa641c8fbf541da7581decdc98667811eb84f0
Soly-M-27 <solymar227@icloud.com>
2021-10-01 00:56:56 +0000

    print without printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <conio.h>
+
 /**
  * main - Prints a string without using puts or printf
  * Return: Returns 1
  */
+
 int main(void)
 {
 
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19,");
-
+	char ch = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char far *pointer = (char far *) 0xb8000000;
+	*pointer = ch;
+	getch();
 	return (1);
 }
