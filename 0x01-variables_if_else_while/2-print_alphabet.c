306e44f2e0cfe842f27a7dcc0d3fe39d30d5d8c3
Soly-M-27 <solymar227@icloud.com>
2021-10-01 19:14:41 +0000

    Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,20 +4,19 @@
  * main - Print alphapet in lower case with putchar
  * Return: Returns 0
  */
-int main(void){
-
+int main(void)
+{
 	int x;
 
-	for(x = 'A'; x <= 'Z'; x++)
+	for (x = 'A'; x <= 'Z'; x++)
 	{
 		int lower_x = tolower(x);
-		putchar(lower_x); 
-		
+putchar (lower_x);
+
 		if (lower_x == 'z')
 		{
-			putchar('\n');		
+			putchar('\n');
 		}
 	}
-
 	return (0);
 }
