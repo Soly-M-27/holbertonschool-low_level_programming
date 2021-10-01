050ff228843729f4a5dbe6d4bd77c57709f78937
Soly-M-27 <solymar227@icloud.com>
2021-10-01 19:23:03 +0000

    Alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,22 +4,25 @@
  * main - Prints alphabet in lower and upper case with putchar
  * Return: Returns 0
  */
-int main(void){
-
+int main(void)
+{
 	int x;
 
-	for(x = 'A'; x <= 'Z'; x++)
+	for (x = 'A'; x <= 'Z'; x++)
 	{
+
 		int lower_x = tolower(x);
+
 		putchar(lower_x);
-		
+
 		if (lower_x == 'z')
 		{
-			for(x = 'a'; x <= 'z'; x++)
+			for (x = 'a'; x <= 'z'; x++)
 			{
 				int upper_x = toupper(x);
+
 				putchar(upper_x);
-				
+
 				if (upper_x == 'Z')
 				{
 					putchar('\n');
