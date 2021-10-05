9c02d8cea3a28cea34cf922a5ebc0b624873e2d0
Soly-M-27 <solymar227@icloud.com>
2021-10-05 20:35:51 +0000

    test 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of any number
+ * @x: holds number in question
+ *
+ * Return: z
+ */
+
+int print_last_digit(int x)
+{
+	int z;
+
+	z = x % 10;
+
+	return (z);
+}
