0dbfeddca0caa61003cb5cf42a6dc47b01653474
Soly-M-27 <solymar227@icloud.com>
2021-10-05 20:07:20 +0000

    Print the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - Prints the absolute value of an integer
+ * @x: hold number is question
+ *
+ * Return: 0
+ */
+
+int _abs(int x)
+{
+	x = abs(x);
+
+	return (x);
+}
