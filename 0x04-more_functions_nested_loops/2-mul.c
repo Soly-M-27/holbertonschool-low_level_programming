8eebd655be48e57b65d9705be0df49bb5361bfc2
Soly-M-27 <solymar227@icloud.com>
2021-10-07 14:20:41 +0000

    Uploading function that multiples two integers and returns it's total successfully

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Function that multiplies two integers
+ * @a: Holds one of the numbers to multiply
+ * @b: Holds another of the numbers to multiply
+ *
+ * Return: mul
+ */
+
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	return (mul);
+}
