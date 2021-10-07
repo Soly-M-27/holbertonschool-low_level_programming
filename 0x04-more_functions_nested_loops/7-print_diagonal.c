3474b47a9d2f93b52de7b5d02b4f29ed07fc40bb
Soly-M-27 <solymar227@icloud.com>
2021-10-07 22:06:44 +0000

    Fixed the new line error

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,30 +9,20 @@
 
 void print_diagonal(int n)
 {
-	int i;
-	int j;
+	int i, j;
 
-	if (n == 0 || n < 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-	else
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		for (j = 0; j < i; j++)
 		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
-				{
-					_putchar('\\');
-				}
-				else
-				{
-					_putchar(' ');
-				}
-			}
-			_putchar('\n');
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
 
