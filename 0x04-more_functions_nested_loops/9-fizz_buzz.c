95bef9ff239f512aa51085cae6a2f4f96806bae7
Soly-M-27 <solymar227@icloud.com>
2021-10-07 19:59:38 +0000

    Upload of program that prints from 1 to 100 and prints a specific word for multiples of 3 and 5

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - Prints 1 to 100 but prints Fizz if # is a multiple of 3, Buzz if from 4, and FizzBuzz it belongs to both
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("%s ", "Buzz");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
