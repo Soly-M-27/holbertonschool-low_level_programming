4e0513584cd600ba1e442c87dd02352f46c62e99
Soly-M-27 <solymar227@icloud.com>
2021-10-11 00:17:06 +0000

    Uploading function that returns the lenth of a string using strlen, found in the <string.h> library

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen = function that returns the length of a string
+ * @s: Holds length
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len;
+	len = strlen(s);
+
+	return (len);
+}
