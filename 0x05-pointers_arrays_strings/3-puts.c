803669b1ab480768d18a19aed7d1b81e87ca8ab1
Soly-M-27 <solymar227@icloud.com>
2021-10-11 00:37:00 +0000

    Uploading function tha prints a string with puts and a new line to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ * @str: Holds the string
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+	_putchar('\n');
+}
