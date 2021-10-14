76bec590f248fef4f1c2638c52133fd3a99e46e6
Soly-M-27 <solymar227@icloud.com>
2021-10-14 20:11:59 +0000

    Upload function that Capitalizes all words of a string with fixed betty and fixed spacing

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,8 +20,8 @@ char *cap_string(char *str)
 				str[x] = str[x] - 32;
 		}
 
-		if (str[x] == '\t')
-			str[x] = ' ';
+		/*if (str[x] == '\t')
+			str[x] = ' ';*/
 
 		while (str[x] == ',' || str[x] == ';' || str[x] == '.' ||
 				str[x] == '!' || str[x] == '?' || str[x] == '"' ||
