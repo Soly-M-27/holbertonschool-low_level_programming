f28892d998e6e868753a2f1694118e37e8ef261d
Soly-M-27 <solymar227@icloud.com>
2021-10-14 19:54:38 +0000

    Upload function that Capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - Function that capitalizes all
+ * words of a string
+ * @str: Holds the string
+ *
+ * Return: char
+ */
+
+char *cap_string(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		if (x == 0)
+		{
+			if(str[x] <= 'a' && str[x] >= 'z')
+				str[x] = str[x] - 32;
+		}
+
+		while (str[x] == ',' || str[x] == ';' || str[x] == '.' ||
+				str[x] == '!' || str[x] == '?' || str[x] == '"' ||
+				str[x] == '(' || str[x] == ')' || str[x] == '{' ||
+				str[x] == '}' || str[x] == ' ' || str[x] == '\t' ||
+				str[x] == '\n')
+		{
+			x++;
+			if (str[x] >= 'a' && str[x] <= 'z')
+				str[x] = str[x] - 32;
+		}
+	}
+	return (str);
+}
+
