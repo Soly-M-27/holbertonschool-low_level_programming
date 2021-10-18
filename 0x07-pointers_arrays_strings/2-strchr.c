60453a3af4665ad9808fe5059e1a025dbb316482
Soly-M-27 <solymar227@icloud.com>
2021-10-18 15:11:41 +0000

    Uploading function that locates a character in a string and prints that string from that character forward

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - Function that locates a character in a strings and
+ * prints rest of the string from the point it finds said character.
+ * @s: Holds the strings in question
+ * @c: Character to find within string
+ *
+ * Return: A pointer to the first occurrence of the character c in
+ * the string s, or NULL if the character is not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int x, len = strlen(s);
+	char *p = s;
+
+	for (x = 0; x < len - 1; x++)
+	{
+		if (s[x] != c)
+			p++;
+	}
+
+	return (p);
+}
