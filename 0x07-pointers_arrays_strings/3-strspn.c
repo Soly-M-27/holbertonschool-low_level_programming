be2cd62ff0bbd0e3915856b91d89d38e69e49f5c
Soly-M-27 <solymar227@icloud.com>
2021-10-18 18:20:36 +0000

    Uploading functions that gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,4 +2,28 @@
 
 /**
  * strspn - Function that gets the length of a prefix substring
- * @s: Hol
+ * @s: Holds first string
+ * @accept: Holds string with letters to be counted from first
+ * string
+ *
+ * Return: Number of bytes in the initial segment of s which
+ * consist only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x = 0;
+	int a, b;
+
+	for (a = 0; accept[a] != '\0'; a++)
+	{
+		for (b = 0; s[b] != '\0'; b++)
+		{
+			if (accept[b] == s[a])
+				x++;
+		}
+	}
+	return (x + 1);
+}
+
+
