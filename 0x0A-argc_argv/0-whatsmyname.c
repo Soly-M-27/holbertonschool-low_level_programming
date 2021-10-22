9bbdb51728fbf8d6ed58087fa3d863c363d0488b
Soly-M-27 <solymar227@icloud.com>
2021-10-22 13:44:30 +0000

    Uploading argc & argv main test

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Main
+ * @argc: Holds an int
+ * @argv: Pointer to array where argc is it's total arguments
+ *
+ * Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
