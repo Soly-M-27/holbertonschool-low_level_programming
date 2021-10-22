d6605fd7486be7c1107dbdba08aac795ca9db00f
Soly-M-27 <solymar227@icloud.com>
2021-10-22 16:59:58 +0000

    Upload all file so far

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  * Return: 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	while (argc--)
-		printf("%s\n", *argv++); 
+		printf("%s\n", *argv++);
 	return (0);
 }
