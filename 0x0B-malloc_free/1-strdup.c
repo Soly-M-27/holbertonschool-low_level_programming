861922688edd73d820b6b42b9dcf83ccaab37732
Soly-M-27 <solymar227@icloud.com>
2021-10-26 16:47:05 +0000

    Uploading 1-strdup.c file whics is a function that duplicates the a string with malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strdup - Function that returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: Holds string
+ *
+ * Return: char pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *str2;
+
+	str2 = malloc(strlen(str) + 1);
+
+	if (str2 == NULL)
+		return (NULL);
+
+	strcpy(str2, str);
+
+	return (str2);
+}
