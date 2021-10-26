a8ee0b06b4131118ef7eaefbc900eff6ec118b18
Soly-M-27 <solymar227@icloud.com>
2021-10-26 17:59:01 +0000

    adding all files

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,8 +20,23 @@ char *str_concat(char *s1, char *s2)
 	if (s3 == NULL)
 		return (NULL);
 
-	strcpy(s3, s1);
-	strcpy(s3 + strlen(s1), s2);
+	if (s1 != NULL && s2 == NULL)
+	{
+		strcpy(s3, s1);
+		strcpy(s3 + strlen(s1));
+	}
+
+	if (s1 == NULL && s2 != NULL)
+	{
+		strcpy(s3, s2);
+		strcpy(s3 + strelen(s2));
+	}
+
+	if (s1 != NULL && s2 != NULL)
+	{
+		strcpy(s3, s1);
+		strcpy(s3 + strlen(s1), s2);
+	}
 
 	return (s3);
 }
