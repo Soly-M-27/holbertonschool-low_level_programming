f617d6c5e70d34b009bc44f9c4f1bfcbc1e797f6
Soly-M-27 <solymar227@icloud.com>
2021-10-26 21:16:59 +0000

    Fixed return NULL issues where now is s1 and s2 are NULL s3 will return an empty string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,36 +13,28 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 
 	if (s1 == NULL && s2 == NULL)
-	{
-		s1 = "";
-		s2 = "";
-
-		s3 = malloc(strlen(s1) + strlen(s2) + 1);
-
-		strcpy(s3, s1);
-		strcpy(s3 + strlen(s1), s2);
-	}
+		return ("");
 
 	if (s1 != NULL && s2 != NULL)
 	{
 		s3 = malloc(strlen(s1) + strlen(s2) + 1);
 
+		if (s3 == NULL)
+			return (NULL);
+
 		strcpy(s3, s1);
 		strcpy(s3 + strlen(s1), s2);
 	}
-
-	if (s1 != NULL && s2 == NULL)
+	else if (s1 != NULL && s2 == NULL)
 	{
-		s2 = "";
-		s3 = malloc(strlen(s1) + strlen(s2) + 1);
+		s3 = malloc(strlen(s1) + 1);
 
 		strcpy(s3, s1);
 	}
 
 	if (s1 == NULL && s2 != NULL)
 	{
-		s1 = "";
-		s3 = malloc(strlen(s2) + strlen(s2) + 1);
+		s3 = malloc(strlen(s2) + 1);
 
 		strcpy(s3, s2);
 	}
