cd15c19062c6bcf3f2b0f52006e775217b1f148b
Soly-M-27 <solymar227@icloud.com>
2021-10-26 17:01:41 +0000

    Uploading 1-strdup.c file whics is a function that duplicates the a string with malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,9 +13,12 @@ char *_strdup(char *str)
 {
 	char *str2;
 
+	if (str == NULL)
+		return (NULL);
+
 	str2 = malloc(strlen(str) + 1);
 
-	if (str2 == NULL && str == NULL)
+	if (str2 == NULL)
 		return (NULL);
 
 	strcpy(str2, str);
