f67f79eca61ff00120a52d1bc62a7a03807ac70c
Soly-M-27 <solymar227@icloud.com>
2021-10-26 18:09:57 +0000

    adding all files

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -9,14 +9,21 @@
 int main(void)
 {
 	char *s;
+	char *c;
+	char *d;
 
 	s = str_concat("Best ", "School");
+	c = str_concat("Hello", NULL);
+	d = str_concat(NULL, "Hello");
+
 	if (s == NULL)
 	{
 		printf("failed\n");
 		return (1);
 	}
 	printf("%s\n", s);
+	printf("%s\n", c);
+	printf("%s\n", d);
 	free(s);
 	return (0);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,27 +15,35 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
-	s3 = malloc(strlen(s1) + strlen(s2) + 1);
+	if (s1 != NULL && s2 != NULL)
+	{
+		s3 = malloc(strlen(s1) + strlen(s2) + 1);
 
-	if (s3 == NULL)
-		return (NULL);
+		if (s3 == NULL)
+			return (NULL);
+
+		strcpy(s3, s1);
+		strcpy(s3 + strlen(s1), s2);
+	}
 
 	if (s1 != NULL && s2 == NULL)
 	{
+		s3 = malloc(strlen(s1) + 1);
+
+		if (s3 == NULL)
+			return (NULL);
+
 		strcpy(s3, s1);
-		strcpy(s3 + strlen(s1), s1);
 	}
 
 	if (s1 == NULL && s2 != NULL)
 	{
-		strcpy(s3, s2);
-		strcpy(s3 + strlen(s2), s2);
-	}
+		s3 = malloc(strlen(s2) + 1);
 
-	if (s1 != NULL && s2 != NULL)
-	{
-		strcpy(s3, s1);
-		strcpy(s3 + strlen(s1), s2);
+		if (s3 == NULL)
+			return (NULL);
+
+		strcpy(s3, s2);
 	}
 
 	return (s3);
