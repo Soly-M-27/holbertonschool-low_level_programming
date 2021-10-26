fe2fd457139b8f2f0c94fea76551aee52e91a168
Soly-M-27 <solymar227@icloud.com>
2021-10-26 17:36:43 +0000

    Add all current files just in case

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * str_concat - Function that concatenates two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: Appended char pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s3;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+
+	s3 = malloc(strlen(s1) + strlen(s2) + 1);
+
+	if (s3 == NULL)
+		return (NULL);
+
+	strcpy(s3, s1);
+	strcpy(s3 + strlen(s1), s2);
+
+	return (s3);
+}
