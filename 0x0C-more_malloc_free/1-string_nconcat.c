9dfc0cf5f5fda6ecd47b9549ec9b46e0c6dfc934
Soly-M-27 <solymar227@icloud.com>
2021-10-28 19:17:45 +0000

    Uploading function that concatenates two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * string_nconcat - Funtion that concatenates two strings
+ * @s1: First string
+ * @s2: second string
+ * @n: unsigned int
+ *
+ * Return: pointer or NULL if malloc fails.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s3;
+	unsigned int x = 0, y, y_len;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (x = 0; s1[x] != '\0'; x++)
+		;
+
+	for (y = 0; s2[y] != '\0'; y++)
+		;
+	y_len = y;
+
+	if (n >= y_len)
+		s3 = malloc(sizeof(char) * x + n);
+	else
+		s3 = malloc(sizeof(char) * x + 1);
+
+	if (s3 == NULL)
+		return (NULL);
+
+	for (x = 0, y = 0; s1[y] != '\0'; x++, y++)
+		s3[x] = s1[y];
+	for (y = 0; s2[y] != '\0' && y < n; y++, x++)
+		s3[x] = s2[y];
+
+	return (s3);
+}
