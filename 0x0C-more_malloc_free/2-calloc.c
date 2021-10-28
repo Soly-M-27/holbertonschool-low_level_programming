e4661efdcf9ca813cb4fa1fa1b3a80336a50f90a
Soly-M-27 <solymar227@icloud.com>
2021-10-28 17:13:00 +0000

    Uploading function that allocates memory for an array, using malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _calloc - Function that allocates memory for an
+ * array, using malloc
+ * @nmemb: memory for an array of nmemb
+ * @size: elements of size bytes
+ *
+ * Return: Pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *s;
+	unsigned int x;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	s = malloc(nmemb * size);
+
+	if (s == NULL)
+		return (NULL);
+
+	while (x < nmemb * size)
+		s[x++] = 0;
+
+	return (s);
+}
