dd8611fbfaba47b76b5967da9207e02aad940826
Soly-M-27 <solymar227@icloud.com>
2021-10-28 14:49:51 +0000

    Add all files so far

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Function that allocates memoery using malloc
+ * @b: unsigned int
+ *
+ * Return: Pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b);
+
+	if (s == NULL)
+	{
+		exit (98);
+	}
+
+	return (s);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <limits.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+#endif /* MAIN_H */
