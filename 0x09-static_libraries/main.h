83f7cf6298605d793044d5bed9df828822dcd2db
Soly-M-27 <solymar227@icloud.com>
2021-10-28 21:01:50 +0000

    add all files

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <math.h>
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -27,5 +21,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr( char *haystack, char *needle);
-
-#endif /* MAIN_H */
