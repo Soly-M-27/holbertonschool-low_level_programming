e785748b04eb794fb747f198eb54510f0f4c0804
Soly-M-27 <solymar227@icloud.com>
2021-11-02 20:27:18 +0000

    Uploading all files for 2-main.c and 2-print_dog.c function

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - Function that prints a struct dog
+ * @d: Pointer to struct
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+	{
+		printf("Name: %s", d->name);
+		putchar('\n');
+	}
+
+	if(d->age == '\0')
+		printf("Age: (nil)\n");
+	else
+	{
+		printf("Age: %f", d->age);
+		putchar('\n');
+	}
+
+	if(d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+	{
+		printf("Owner: %s", d->owner);
+		putchar('\n');
+	}
+}
