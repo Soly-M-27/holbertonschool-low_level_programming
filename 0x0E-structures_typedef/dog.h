5575e6c0084e6851560901a3f01f5f6676183335
Soly-M-27 <solymar227@icloud.com>
2021-11-02 23:53:20 +0000

    Adding new struct for new dog info

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,22 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - New struct for new dog
+ * @name: Holds name of pet
+ * @age: Holds age of pet
+ * @owner: Holds owner of pet
+ *
+ * Description - New Dog Struct
+ */
+
+struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
