90f690ca61b04e8dde44723d4f0f7d196bb559c3
Soly-M-27 <solymar227@icloud.com>
2021-11-03 01:27:56 +0000

    Reupload test

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,7 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG
+#define DOG
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - Gives name, age and owner of pet
@@ -17,14 +19,24 @@ struct dog
 	char *owner;
 };
 
-typedef struct
+/**
+ * struct - Dog description
+ * @name: Holds dogs name
+ * @age: Holds dogs age
+ * @owner: Holds owners name
+ */
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
-#endif
+#endif /* DOG */
