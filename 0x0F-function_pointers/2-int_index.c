29b736c20423585dea52e60e7e234e12e830f563
Soly-M-27 <solymar227@icloud.com>
2021-11-03 22:28:45 +0000

    Uploading function that returns position of array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - Function that searches for an integer
+ * @array: Holds array
+ * @size: Holds size of array
+ * @cmp: Function pointer to display array position
+ *
+ * Return: int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (size <= 0)
+		return -1;
+
+	if ((array == NULL) || (cmp == NULL))
+		return (-1);
+
+	for (x = 0; x < size; x++)
+	{
+		if (cmp(array[x]) != 0)
+			return (x);
+	}
+	return (-1);
+}
