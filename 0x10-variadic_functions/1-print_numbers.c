379d57be838eeb01d740ed54724adce7c7b585bc
Soly-M-27 <solymar227@icloud.com>
2021-11-04 21:05:00 +0000

    uploading function that prints numbers separated by a coma with variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,15 +16,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int x = 0, z;
 
-	if (separator == NULL)
-		return;
-
 	va_start(args, n);
 	for (z = 0; z < n; z++)
 	{
-		x = va_arg(args, int);
+		x = va_arg(args, unsigned int);
 		printf("%d", x);
-		if ((z + 1) != n && separator != NULL)
+		if ((z + 1) != n && (separator != NULL))
 			printf("%s", separator);
 	}
 	printf("\n");
