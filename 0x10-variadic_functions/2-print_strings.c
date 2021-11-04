03fe849036a048ded45ffd508c850c876460529e
Soly-M-27 <solymar227@icloud.com>
2021-11-04 21:48:33 +0000

    Uploading function that prints strong with a separator using variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,11 +24,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (ch != NULL)
 			printf("%s", ch);
 		else
-			printf("nil");
+			printf("(nil)");
 
 		if ((x + 1) != n && (separator != NULL))
 			printf("%s", separator);
 	}
+
 	printf("\n");
+
 	va_end(strings);
 }
