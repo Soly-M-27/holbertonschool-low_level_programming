41c585b7886a49124e7f8b5a885b4ffe35dc4611
Soly-M-27 <solymar227@icloud.com>
2021-11-12 20:31:33 +0000

    Add all files

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,8 +2,8 @@
 #define LISTS_H
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -23,5 +23,6 @@ typedef struct list_s
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif /* LISTS_H */
