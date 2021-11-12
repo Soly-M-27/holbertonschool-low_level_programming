1959cfcfcd69723e8b6617cc5eb0ddfeaa7fa34f
Soly-M-27 <solymar227@icloud.com>
2021-11-12 20:20:12 +0000

    Fixed betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,13 +12,15 @@ size_t print_list(const list_t *h)
 {
 	const list_t *n;
 	size_t x = 0;
+
 	n = h;
+
 	while (n != NULL)
 	{
 		if (n->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%i] %s\n", n->len ,n->str);
+			printf("[%i] %s\n", n->len, n->str);
 		n = n->next;
 		x++;
 	}
