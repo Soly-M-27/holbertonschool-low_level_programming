135790574ec196954db9704d3749f13e562cfe44
Soly-M-27 <solymar227@icloud.com>
2021-11-16 17:47:27 +0000

    Add all files

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Function that deletes the head node
+ * of a listint_t linked list, and returns the head
+ * node's data (n).
+ * @head: Head of the list
+ *
+ * Return: Return linked list or 0 if list is empty
+ */
+
+int pop_listint(listint_t **head)
+{
+
+}
