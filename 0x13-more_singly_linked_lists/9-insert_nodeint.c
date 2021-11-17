5dc65bdc6ff96860c9f1fbf8dac481c6206889d8
Soly-M-27 <solymar227@icloud.com>
2021-11-17 14:14:25 +0000

    Uploading function that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,5 +41,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		newNode->next = insert->next;
 		insert->next = newNode;
 	}
-	return (newNode;
+	return (newNode);
 }
