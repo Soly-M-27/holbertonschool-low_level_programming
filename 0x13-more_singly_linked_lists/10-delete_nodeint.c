52607cf3a3a9c869c986c0c80920a4917ea8026f
Soly-M-27 <solymar227@icloud.com>
2021-11-17 14:27:01 +0000

    Uploading function that deletes the node at specified index of a listint_t linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			return (1);
 		}
 
-		for (x = 1; tmp != NULL && x != index - 1; x++)
+		for (x = 0; tmp != NULL && x < index - 1; x++)
 			tmp = tmp->next;
 
 		if (tmp == NULL || tmp->next == NULL)
