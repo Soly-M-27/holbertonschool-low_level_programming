72bb4c9b747add4b628eb658a678ee8168cd19a2
Soly-M-27 <solymar227@icloud.com>
2021-11-22 04:09:33 +0000

    Uploading function that prints the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_binary - function that prints the binary
+ * representation of a number
+ * @n: number
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n / 2);
+
+	printf("%ld", n % 2);
+}
+
