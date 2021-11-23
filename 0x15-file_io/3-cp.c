814346f2c5e925c9a7edae93ab9e166d255c89fb
Soly-M-27 <solymar227@icloud.com>
2021-11-23 15:38:15 +0000

    Reuploading function that copies contents of a file into another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int cp(char *f_from, char *f_to);
+void cp_file(char *f_from, char *f_to);
 
 /**
  * main - Program that copies the content of a file
@@ -10,28 +10,16 @@ int cp(char *f_from, char *f_to);
  *
  * Return: 0
  */
+
 int main(int ac, char **av)
 {
-	int x;
-
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	x = cp(av[1], av[2]);
-
-	if (x == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	else if (x == -2)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+	cp_file(av[1], av[2]);
 
 	return (0);
 }
@@ -42,43 +30,42 @@ int main(int ac, char **av)
  * @f_from: First File
  * @f_to: Second File
  *
- * Return: 1 if successful, -1 or -2 depending
- * on the error code
+ * Return: void
  */
 
-int cp(char *f_from, char *f_to)
+void cp_file(char *f_from, char *f_to)
 {
 	int from_NO, to_NO, x, z, c1, c2;
 	char tmp[BUFSIZ];
 
 	if (f_from == NULL || f_to == NULL)
-		return (0);
-
+		return;
 	from_NO = open(f_from, O_RDONLY);
 	x = read(from_NO, tmp, BUFSIZ);
 
 	if (from_NO == -1 || x < 0)
-		return (-1);
-
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f_from);
+		exit(98);
+	}
 	to_NO = open(f_to, O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	z = write(to_NO, tmp, x);
 
 	if (to_NO == -1 || x != z)
-		return (-2);
-
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %d\n", to_NO);
+		exit(99);
+	}
 	c1 = close(from_NO);
 	if (c1 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_NO);
 		exit(100);
 	}
-
 	c2 = close(to_NO);
 	if (c2 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_NO);
 		exit(100);
 	}
-
-	return (1);
 }
