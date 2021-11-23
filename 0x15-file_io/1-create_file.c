4a274cc899e05aa1f067ce3e86885129b96b8be2
Soly-M-27 <solymar227@icloud.com>
2021-11-23 04:09:53 +0000

    Uploading function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * @text_content: NULL terminated string to write to
  * the file
  *
-  * Return: 1 on success, -1 on failure
+ * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
@@ -21,11 +21,11 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
-	for (x = 0; text_content[x] != '\0'; x++)
-		;
-
 	if (text_content != NULL)
 	{
+		for (x = 0; text_content[x] != '\0'; x++)
+			;
+
 		z = write(fd, text_content, x);
 		if (x != z)
 		{
