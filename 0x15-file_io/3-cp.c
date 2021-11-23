7df365002413febafb62b15daeec6e97551495b4
Soly-M-27 <solymar227@icloud.com>
2021-11-23 05:33:36 +0000

    Program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int cp(char *f_from, char *f_to);
+
 /**
  * main - Program that copies the content of a file
  * to another file
@@ -8,44 +10,75 @@
  *
  * Return: 0
  */
-
 int main(int ac, char **av)
 {
-	int f_f, f_t;
+	int x;
 
 	if (ac != 3)
 	{
 		exit(97);
-		printf("Usage: cp file_from file_to\n");
+		dprintf(2, "Usage: cp file_from file_to\n");
 	}
 
-	f_t = open("file_to", O_CREATE | O_TRUNC, 00664);
+	x = cp(av[1], av[2]);
 
-	f_f = open("file_from", O_RDONLY);
-
-	if (!f_f)
+	if (x == -1)
 	{
 		exit(98);
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 	}
-
-	read(f_f, av[1], 1024);
-	write(STDOUT_FILENO, f_t, 1024);
-
-	close(f_t);
-	close(f_f);
-
-	if (!(close(f_f)))
+	else if (x == -2)
 	{
-		exit(100);
-		dprintf(2, "Error: Can't close fd %s\n", av[1]);
-	}
-
-	if (!(close(f_t)))
-	{
-		exit(100);
-		dprintf(2, "Error: Can't close fd %s\n" av[2]);
+		exit(99);
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
 	}
 
 	return (0);
+}
+
+/**
+ * cp - Function that copies contents of file
+ * into another file
+ * @f_from: First File
+ * @f_to: Second File
+ *
+ * Return: 1 if successful, -1 or -2 depending
+ * on the error code
+ */
+
+int cp(char *f_from, char *f_to)
+{
+	int from_NO, to_NO, x, z, c1, c2;
+	char tmp[BUFSIZ];
+
+	if (f_from == NULL || f_to == NULL)
+		return (0);
+
+	from_NO = open(f_from, O_RDWR);
+	x = read(from_NO, tmp, BUFSIZ);
+
+	if (from_NO == -1 || x < 0)
+		return (-1);
+
+	to_NO = open(f_to, O_CREAT | O_TRUNC | O_RDWR, 00664);
+	z = write(to_NO, tmp, x);
+
+	if (to_NO == -1 || x != z)
+		return (-2);
+
+	c1 = close(from_NO);
+	if (c1 < 0)
+	{
+		exit(100);
+		dprintf(2, "Error: Can't close fd %d\n", from_NO);
+	}
+
+	c2 = close(to_NO);
+	if (c2 < 0)
+	{
+		exit(100);
+		dprintf(2, "Error: Can't close fd %d\n", from_NO);
+	}
+
+	return (1);
 }
