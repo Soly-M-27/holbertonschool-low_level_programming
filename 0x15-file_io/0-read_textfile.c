9cd0f330882d1fd3ec744529ba7737bd02e1f080
Soly-M-27 <solymar227@icloud.com>
2021-11-23 00:52:27 +0000

    Uploading function that reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * read_textfile - Function that reads a text file and prints it
+ * to the POSIX standard output
+ * @filename: Name of file
+ * @letters: Amount of letters to read and print
+ *
+ * Return: if write fails or does not write the expected amount
+ * of bytes, return 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int rf;
+	char *mal = malloc(sizeof(int) * letters);
+
+	if (filename == NULL)
+		return (0);
+
+	rf = open("Requiescat", O_RDONLY);
+
+	if (rf == -1)
+		return (0);
+
+	read(rf, mal, letters);
+
+	if (mal == NULL)
+		return (0);
+
+	write(STDOUT_FILENO, mal, letters);
+
+	return (letters);
+}
