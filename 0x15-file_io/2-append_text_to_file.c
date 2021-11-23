4347fa4b4ac0f912ce6da9d07a52b4a45b9f7d04
Soly-M-27 <solymar227@icloud.com>
2021-11-23 04:26:32 +0000

    Uploading function that appends text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,23 +12,28 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, x;
+	int fd, x, z;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		return (1);
-
-	fd = open(filename, O_CREAT | O_APPEND, 00664);
+	fd = open(filename, O_APPEND | O_RDWR);
 
 	if (fd == -1)
 		return (-1);
 
-	for (x = 0; text_content[x] != '\0'; x++)
-		;
+	if (text_content != NULL)
+	{
+		for (x = 0; text_content[x] != '\0'; x++)
+			;
 
-	write(STDOUT_FILENO, filename, x);
+		z = write(fd, text_content, x);
+		if (x != z)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
 	close(fd);
 
