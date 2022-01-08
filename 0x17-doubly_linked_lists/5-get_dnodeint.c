6bf8119ad24611c11cc3a6893f2f250bc31e0afd
Soly-M-27 <solymar227@icloud.com>
2022-01-08 03:53:58 +0000

    function that returns the nth node of a doubly linked list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,7 +1,12 @@
 #include "lists.h"
 
 /**
+ * get_dnodeint_at_index - Function that returns the nth
+ * node of a dlistint_t linked list.
+ * @head: head of the list
+ * @index: index to be added
  *
+ * Return: if the node does not exist, return NULL
  */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
