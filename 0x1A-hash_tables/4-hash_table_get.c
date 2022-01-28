029a8730ddc8cbfc952e477843bdadcea88ce8e9
Soly-M-27 <solymar227@icloud.com>
2022-01-28 21:03:55 +0000

    Fixed betty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -26,7 +26,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	while (checker != NULL)
 	{
 		if (strcmp(key, checker->key) == 0)
-			result = checker->key;
+			result = checker->value;
 
 		checker = checker->next;
 	}
