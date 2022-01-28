231582fd58c7517507619db0f91956b0745a5720
Soly-M-27 <solymar227@icloud.com>
2022-01-28 18:11:27 +0000

    Function that adds an element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Function that adds an element to the hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value
+ *
+ * Return: 1 if succeeded, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int x = 0, total_size;
+	hash_node_t *added_node = malloc(sizeof(hash_node_t));
+	char *empty = "";
+
+	if (added_node == NULL)
+		return (0);
+
+	if (key == empty || key == NULL)
+		return (0);
+
+	total_size = ht->size;
+	added_node->key = strdup(key);
+	added_node->value = strdup(value);
+
+	for (; x < total_size; x++)
+	{
+		ht->array[x] = added_node;
+	}
+
+	return (1);
+}
