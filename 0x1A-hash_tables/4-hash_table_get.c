aca7ed9326c46d1ed9863473d384864fc7ea293e
Soly-M-27 <solymar227@icloud.com>
2022-01-28 21:00:29 +0000

    Function that retrieves a value associated with a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Function that retrieves a value associated
+ * with a key
+ * @ht: hash table
+ * @key: key
+ *
+ * Return: value associated with the element, or NULL if key coudn't
+ * be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int id;
+	char *result, *empty = "";
+	hash_node_t *checker;
+
+	if (ht == NULL || key == empty)
+		return (NULL);
+
+	id = key_index((const unsigned char *)key, ht->size);
+
+	checker = ht->array[id];
+
+	while(checker != NULL)
+	{
+		if (strcmp(key, checker->key) == 0)
+			result = checker->key;
+
+		checker = checker->next;
+	}
+
+	return (result);
+}
