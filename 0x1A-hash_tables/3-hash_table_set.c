8e967a1e94ba6d3d2cbb775be58d1ad944f3fbcd
Soly-M-27 <solymar227@icloud.com>
2022-01-29 03:23:45 +0000

    Fixed betty and function added

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,25 +12,36 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int x;
-	hash_node_t *added_node = malloc(sizeof(hash_node_t));
 	char *empty = "";
+	hash_node_t *added_node;
 	char *copy_value, *copy_key;
+
+	added_node = malloc(sizeof(hash_node_t));
 
 	if (key == empty || key == NULL || value == NULL ||
 			ht == NULL || added_node == NULL)
 		return (0);
-
 	copy_value = strdup(value);
+	if (copy_value == NULL)
+	{
+		free(added_node);
+		return (0);
+	}
 	copy_key = strdup(key);
-
+	if (copy_key == NULL)
+	{
+		free(added_node);
+		free(copy_value);
+		return (0);
+	}
 	added_node->key = copy_key;
 	added_node->value = copy_value;
 	x = key_index((const unsigned char *)key, ht->size);
-
 	if (ht->array[x] == NULL)
-		ht->array[x] = added_node;
+		added_node->next = NULL;
 	else
 		added_node->next = ht->array[x];
-
+	ht->array[x] = added_node;
+	/*hash_table_print((const hash_table_t *)ht);*/
 	return (1);
 }
