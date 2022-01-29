b4c9296f34829f73b12f8a3a46c4439f4cd1fba5
Soly-M-27 <solymar227@icloud.com>
2022-01-29 02:25:13 +0000

    Function that prints a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Function that prints a hash table
+ * @ht = hash table
+ *
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+
+	unsigned long int x = 0;
+	char *string = "";
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (; x < ht->size; x++)
+	{
+		while (ht->array[x] != NULL)
+		{
+			printf("%s'%s': '%s'", string, ht->array[x]->key, ht->array[x]->value);
+			string = ", ";
+			ht->array[x] = ht->array[x]->next;
+		}
+	}
+	printf("}");
+	printf("\n");
+}
